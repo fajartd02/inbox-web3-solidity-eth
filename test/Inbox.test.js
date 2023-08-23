f1a286bc9bd42c9d1aa79954fca4ccb7c284b8cd
fajartd02 <fajarmh2@gmail.com>
2023-08-24 01:12:37 +0700

    add provider and instance web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,7 @@
 const { Web3 } = require('web3');
 const ganache = require('ganache');
 const assert = require('assert');
-
+const web3 = new Web3(ganache.provider());
 // updated ganache and web3 imports added for convenience
 
 // contract test code will go here
