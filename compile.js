cfd24c0e7125987ff18ca69dc1be3d87ca1407af
fajartd02 <fajarmh2@gmail.com>
2023-08-25 00:22:14 +0700

    feat: adding test ABI  & bytecode compile deploy contract

--- compile.js
+++ compile.js
@@ -1,17 +1,8 @@
-/* AUTHOR: FAJAR MUHAMMAD HAMKA ***********************************
-NOTES:                                                            *
-    Contract Source -> Solidity Compiler                          *
-    Solidty Compiler will create 2 -> ABI & Contract Bytecode     *
-    ABI: Javascript Interpretation Layer of what the contract is  *
-    Bytecode: Deploy off to the thereum network                   *
-*******************************************************************/
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
 
-// compile code will go here
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
-
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,25 @@
-const { Web3 } = require('web3');
-const ganache = require('ganache');
 const assert = require('assert');
+const ganache = require('ganache');
+const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
-// updated ganache and web3 imports added for convenience
+const { interface, bytecode } = require('../compile');
 
-// contract test code will go here
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+ accounts = web3.eth.getAccounts();
+  
+  // Use on of those accounts to deploy
+  // The contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+          .deploy({data: bytecode, arguments: ['Hi There!']})
+          .send({ from: accounts[0], gas: '1000000' });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
