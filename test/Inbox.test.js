c0a37fe77e208bcca7aac6b56af1526caf95206f
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-09-13 14:52:52 +0700

    $npm_config_message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,27 +1,27 @@
-const assert = require('assert');
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
-const INITIAL_STRING = 'Hi There!';
+const INITIAL_STRING = "Hi There!";
 
 beforeEach(async () => {
   // Get a list of all accounts
- accounts = web3.eth.getAccounts();
-  
+  accounts = await web3.eth.getAccounts();
+
   // Use on of those accounts to deploy
   // The contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-          .deploy({data: bytecode, arguments: [INITIAL_STRING]})
-          .send({ from: accounts[0], gas: '1000000' });
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    assert.ok(inbox.options.address)
+    assert.ok(inbox.options.address);
   });
 
   it("has a default message", async () => {
@@ -29,9 +29,9 @@ describe("Inbox", () => {
     assert.equal(message, INITIAL_STRING);
   });
 
-  it('can change the message', async () => {
-    await inbox.methods.setMessage('bye').send({ from: accounts[0] }); // get return transaction hash
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] }); // get return transaction hash
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'bye');
-  })
+    assert.equal(message, "bye");
+  });
 });
